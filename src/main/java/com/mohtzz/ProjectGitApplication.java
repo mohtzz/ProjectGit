aa6468e61b45caf74081b29a85c44d978aa0a2ce
mohtzz <peschanov1103@mail.ru>
2024-01-16 22:28:37 +0400

    theres a problem

--- src/main/java/com/mohtzz/ProjectGitApplication.java
+++ src/main/java/com/mohtzz/ProjectGitApplication.java
@@ -4,7 +4,7 @@ public class ProjectGitApplication {
     public static void main(String[] args) {
 
         int x = 1;
-        int y = 5;
+        int y = 6c;
         System.out.println("Hello git!!");
 
     }
