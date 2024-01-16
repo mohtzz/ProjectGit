a5f60eba77e8f6cb49d0f44ca909f7cb13b0af61
mohtzz <peschanov1103@mail.ru>
2024-01-16 21:05:42 +0400

    Feature 2 commit

--- src/main/java/com/mohtzz/ProjectGitApplication.java
+++ src/main/java/com/mohtzz/ProjectGitApplication.java
@@ -4,7 +4,7 @@ public class ProjectGitApplication {
     public static void main(String[] args) {
 
         int x = 1;
-        int y = 0;
+        int y = 1;
         System.out.println("Hello git!!");
 
     }
