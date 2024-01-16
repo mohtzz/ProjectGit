d25aa9c79ab148d9942bffaa884836a39c2e2bf9
mohtzz <peschanov1103@mail.ru>
2024-01-16 19:44:20 +0400

    second commit

--- src/main/java/com/mohtzz/ProjectGitApplication.java
+++ src/main/java/com/mohtzz/ProjectGitApplication.java
@@ -2,6 +2,9 @@ package com.mohtzz;
 
 public class ProjectGitApplication {
     public static void main(String[] args) {
+
+        int x = 0;
         System.out.println("Hello git!!");
+
     }
 }
